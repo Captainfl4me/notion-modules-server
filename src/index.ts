9ad49e5b71337e00f720521513d90142538cb624
Nicolas Thierry <nicolas.thierry.perso@gmail.com>
2023-01-18 22:15:25 +0100

    set up calendar module

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-console.log('Hello world!')
+import { Client } from '@notionhq/client';
+import { SyncCalendarModule } from 'sync-calendar-module';
+import { ModuleSettings } from 'sync-calendar-module/build/settings.helper';
+
+import { NotionAPI, DatabaseID, SettingsDatabaseID } from './settings.json';
+ 
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+})
+
+const calendarModuleSettings = new ModuleSettings();
+calendarModuleSettings.NotionAPI = NotionAPI;
+calendarModuleSettings.DatabaseID = DatabaseID;
+calendarModuleSettings.SettingsDatabaseID = SettingsDatabaseID;
+
+const calendarModule = new SyncCalendarModule(calendarModuleSettings);
+
+setInterval(calendarModuleLoop, 1000*60);
+
+let updateCalendarModuleTimer = 0;
+
+function calendarModuleLoop(){
+  updateCalendarModuleTimer++;
+  if(updateCalendarModuleTimer >= 30) updateCalendarModule();
+}
+
+async function updateCalendarModule(){
+  updateCalendarModuleTimer = 0;
+
+  const calendarSettings = await calendarModule.getNotionCalendarSettings(notion);
+  for(let i = 0; i < calendarSettings.length; i++){
+    await calendarModule.updateACalendar(notion, calendarSettings[i].properties, calendarSettings[i].id);
+  }
+}
